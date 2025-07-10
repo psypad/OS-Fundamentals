529e8ec0bc351b6571c62c9484cf562ece1d0150
psypad <psypad20@gmail.com>
2025-07-10 12:15:09 +0530

    printing to kernelfunctions completed

--- NeuOS/src/kernel.c
+++ NeuOS/src/kernel.c
@@ -1,16 +1,77 @@
 #include "kernel.h"
 #include <stdint.h>
+#include <stddef.h>
 
-void kernel_main(){
-    //uint16_t* video_mem = (uint16_t*)(0xB8000);
-    //video_mem[0] = 0x0341;
+uint16_t* video_mem = 0;
+uint16_t terminal_row = 0;
+uint16_t terminal_col = 0;
 
-    char* videoIter  = (char*)(0xB8000);
+uint16_t terminal_make_char(char c, char colour)
+{
+    return (colour << 8) | c;
+}
 
-    char vec[24] = {'H', 3 ,'E', 3, 'L', 3 ,'L', 3, 'L', 3, 'O', 3, ' ', 3, 'W', 3, 'O', 3, 'R', 3, 'L', 3, 'D', 3};
+void terminal_putchar(int x, int y, char c, char colour)
+{
+    video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, colour);
+}
 
-    for(int i = 0; i < 25;i++){
-        videoIter[i] = vec[i];
+void terminal_writechar(char c, char colour)
+{
+    if(c == '\n')
+    {
+        terminal_row += 1;
+        terminal_col = 0;
+        return;
     }
+
+    terminal_putchar(terminal_col,terminal_row,c, colour);
+    terminal_col += 1;
+    if(terminal_col >= VGA_WIDTH)
+    {
+        terminal_col = 0;
+        terminal_row += 1;
+    }
+}
+
+void terminal_init()
+{
+    video_mem = (uint16_t*)(0xB8000);
+    terminal_row = 0;
+    terminal_col = 0;
+    for(int y = 0; y <VGA_HEIGHT; y++)
+    {
+        for(int x = 0; x<VGA_WIDTH; x++)
+        {
+            terminal_putchar(x,y,' ', 0);
+        }
+    }
+}
+
+size_t strlen(const char* str)
+{
+    size_t len = 0;
+    while(str[len])
+    {
+        len++;
+    }
+
+    return len;
+}
+
+void print(const char* str)
+{
+    size_t len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        terminal_writechar(str[i], 15);
+    }
+}
+
+void kernel_main()
+{
+
+    terminal_init();
+    print("Hello world!\ntest");
 
 }
